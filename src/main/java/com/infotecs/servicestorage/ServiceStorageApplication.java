5811ca727f37934f7a557a0b267ae11442ca4c46
kostyan87 <kostya.ignatev.14@mail.ru>
2021-08-25 21:08:15 +0300

    Создан проект

---
+++ src/main/java/com/infotecs/servicestorage/ServiceStorageApplication.java
@@ -0,0 +1,13 @@
+package com.infotecs.servicestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/infotecs/servicestorage/ServiceStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotecs.servicestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
